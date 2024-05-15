aee068bc33d7e7c8d7613e275a7063c50f432b8d
littlepure2333 <rz8217525@163.com>
2024-05-15 21:33:37 +0800

    update benchmarking

--- gflow/fit_video.py
+++ gflow/fit_video.py
@@ -12,6 +12,25 @@ from helper import readFlow
 
 # TODO background augmentation
 
+
+def print_color(msg, color="green"):
+    if color == "red":
+        print("\033[91m {}\033[00m".format(msg))
+    elif color == "green":
+        print("\033[92m {}\033[00m".format(msg))
+    elif color == "yellow":
+        print("\033[93m {}\033[00m".format(msg))
+    elif color == "blue":
+        print("\033[94m {}\033[00m".format(msg))
+    elif color == "purple":
+        print("\033[95m {}\033[00m".format(msg))
+    elif color == "cyan":
+        print("\033[96m {}\033[00m".format(msg))
+    elif color == "white":
+        print("\033[97m {}\033[00m".format(msg))
+    else:
+        print(msg)
+
 def main(
     height: int = 256,
     width: int = 256,
@@ -31,6 +50,7 @@ def main(
     lr_camera_after: float = 1e-3,
     gpu: int = 0,
     ckpt_path: Optional[str] = None,
+    lambda_rgb: float = 1.,
     lambda_depth: float = 0.,
     lambda_still: float = 0.,
     lambda_delta: float = 0.,
@@ -47,6 +67,7 @@ def main(
     grad_threshold: float = 5e-3,
     densify_times_after: int = 1,
     densify_interval_after: int = 0,
+    densify_iter: int = 0,
     grad_threshold_after: float = 5e-3,
     densify_times_new: int = 1,
     densify_interval_new: int = 0,
@@ -56,12 +77,13 @@ def main(
     resize: int = None,
     blur: bool = False,
     loss_verbose: bool = False,
-    add: bool = False,
-    depth_scale: float = 16.0,
-    depth_offset: float = 2.0,
+    depth_scale: float = 1.0,
+    depth_offset: float = 0.,
     num_knn: int = 20,
     slow_color: bool = False,
     slow_means: bool = False,
+    traj_num: int = 0,
+    traj_offset: int = 0,
 ) -> None:
     
     frames_sequence_optimize = []
@@ -74,6 +96,8 @@ def main(
     frames_still_center_sequence = []
     frames_move_sequence = []
     frames_move_center_sequence = []
+    frames_sequence_traj = []
+    frames_sequence_traj_upon = []
 
     # read all images(png, jpg, ...) in the folder and sort them
     img_paths = sorted(Path(sequence_path).glob("*.png")) + sorted(Path(sequence_path).glob("*.jpg"))
@@ -87,13 +111,11 @@ def main(
     img_occ_paths = sorted(Path(str(sequence_path)+'_flow').glob("*occ_bwd.png")) + sorted(Path(str(sequence_path)+'_flow').glob("*occ_bwd.jpg"))
     img_occ_paths = img_occ_paths[frame_start:frame_start+frame_range-1][::skip_interval]
     flow_paths = sorted(Path(str(sequence_path)+'_flow').glob("*pred.flo"))
-    flow_paths = flow_paths[frame_start:frame_start+frame_range-1][::skip_interval]
+    flow_paths = flow_paths[frame_start:frame_start+frame_range][::skip_interval]
     camera_paths = sorted(Path(str(sequence_path)+'_camera_dust3r').glob("*.json"))
-    camera_paths = camera_paths[frame_start:frame_start+frame_range-1][::skip_interval]
+    camera_paths = camera_paths[frame_start:frame_start+frame_range][::skip_interval]
     focal, pp, extr_list = read_camera(camera_paths)
-    # print(focal)
-    # print(pp)
-    # print(extr_list[0])
+    depth_scale = focal / 20
     
 
     # gt_images = [image_path_to_tensor(img_path, resize=resize, blur=blur) for img_path in img_paths]
@@ -104,11 +126,10 @@ def main(
     save_name0 = os.path.basename(img_paths[0]).split('.')[0]
     gt_image0 = image_path_to_tensor(img_paths[0], resize=resize, blur=blur)
     # gt_depth0 = image_path_to_tensor(img_depth_paths[0], resize=resize, blur=blur)
-    gt_depth0 = read_depth(img_depth_paths[0], resize=resize).unsqueeze(-1)
-
-    trainer = SimpleGaussian(gt_image=gt_image0, gt_depth=gt_depth0, 
-                             num_points=num_points, background=background, 
-                             depth_scale=depth_scale, depth_offset=depth_offset)
+    gt_depth0 = read_depth(img_depth_paths[0], resize=resize, depth_scale=depth_scale, depth_offset=depth_offset).unsqueeze(-1)
+    gt_flow0 = readFlow(flow_paths[0], resize=resize, blur=blur)
+    trainer = SimpleGaussian(gt_image=gt_image0, gt_depth=gt_depth0, gt_flow=gt_flow0,
+                             num_points=num_points, background=background)
     trainer.load_camera(focal, pp)
     # trainer.load_camera(focal, pp, extr_list[0])
     trainer.init_gaussians_from_image(gt_image=gt_image0, gt_depth=gt_depth0, num_points=num_points)
@@ -122,6 +143,7 @@ def main(
         save_videos=True,
         save_ckpt=True,
         ckpt_name=save_name0,
+        lambda_rgb=lambda_rgb,
         lambda_depth=lambda_depth,
         lambda_var=lambda_var,
         densify_times=densify_times,
@@ -139,18 +161,33 @@ def main(
         frames_still_center_sequence.append(still_center_np)
         frames_move_sequence.append(move_rgb_np)
         frames_move_center_sequence.append(move_center_np)
+    
+    # render trajectory
+    if traj_num:
+        num_points = len(trainer.get_attribute("xyz"))
+        interval = int(num_points / traj_num)
+        traj_index = range(num_points)[traj_offset::interval]
+        (out_img, out_img_center, out_img_depth, 
+         out_img_traj, out_img_traj_upon ) = trainer.eval(
+            traj_index=traj_index,
+            line_scale=0.2, 
+            point_scale=2., 
+            alpha=0.8
+        )
+        frames_sequence_traj.append(out_img_traj)
+        frames_sequence_traj_upon.append(out_img_traj_upon)
 
     # fit the subsequent frames
     # for img_path in img_paths[1:]:
     for i, img_path in enumerate(img_paths[1:]):
         save_name = os.path.basename(img_path).split('.')[0]
-        print(f"[{i+1}/{len(img_paths) - 1}] fitting {img_path}")
+        print_color(f"[{i+1}/{len(img_paths) - 1}] fitting {img_path}")
         trainer.load_checkpoint(trainer.checkpoint_path)
         gt_image = image_path_to_tensor(img_path, resize=resize, blur=blur)
         # gt_depth = image_path_to_tensor(img_depth_paths[i+1], resize=resize, blur=blur)
-        gt_depth = read_depth(img_depth_paths[i+1], resize=resize).unsqueeze(-1)
+        gt_depth = read_depth(img_depth_paths[i+1], resize=resize, depth_scale=depth_scale, depth_offset=depth_offset).unsqueeze(-1)
         occ_mask = image_path_to_tensor(img_occ_paths[i], resize=resize, blur=blur)
-        gt_flow = readFlow(flow_paths[i], resize=resize, blur=blur)
+        gt_flow = readFlow(flow_paths[i+1], resize=resize, blur=blur)
         # transform 1 is near to 0 is near
         # gt_depth = (1 - gt_depth) * depth_scale + depth_offset
         trainer.set_gt_image(gt_image)
@@ -158,9 +195,10 @@ def main(
         trainer.set_gt_flow(gt_flow)
 
         if camera_first:
-            print(f"[{i+1}/{len(img_paths) - 1}] fitting camera-only first.................")
+            print_color(f"[{i+1}/{len(img_paths) - 1}] fitting camera-only first.................")
             frames, frames_center, frames_depth, still_rgb_np, still_center_np, move_rgb_np, move_center_np = trainer.train(
                 iterations=iterations_camera,
+                # lr=lr,
                 lr=0.,
                 # lr_rgbs=lr_rgbs,
                 lr_camera=lr_camera_after,
@@ -168,11 +206,12 @@ def main(
                 save_videos=False,
                 save_ckpt=True,
                 ckpt_name=save_name,
+                lambda_rgb=0.,
                 lambda_depth=lambda_depth,
                 # lambda_depth=0.,
                 lambda_var=lambda_var,
                 lambda_still=0.,
-                lambda_flow=0.,
+                lambda_flow=lambda_flow,
                 densify_times=densify_times,
                 densify_interval=densify_interval,
                 grad_threshold=grad_threshold,
@@ -185,34 +224,35 @@ def main(
             # frames_center_sequence.append(frames_center[-1])
             # frames_depth_sequence.append(frames_depth[-1])
 
-            print("[check] scales' max, min", trainer.get_attribute("scale").max().item(), trainer.get_attribute("scale").min().item())
-            print("[check] depth_scale", trainer.depth_scale)
-            print("[check] depth far, near", trainer.last_depth[trainer.last_depth>0].max(), trainer.last_depth[trainer.last_depth>0].min())
-            print("[check] camera intr: \n", trainer.intr)
-            print("[check] camera extr: \n", trainer.extr)
+            print_color(f"[check] scales' max, min: {trainer.get_attribute('scale').max().item()} {trainer.get_attribute('scale').min().item()}")
+            # print_color("[check] depth_scale", trainer.depth_scale)
+            print_color(f"[check] depth far, near {trainer.last_depth[trainer.last_depth>0].max()} {trainer.last_depth[trainer.last_depth>0].min()}")
+            print_color(f"[check] camera intr: \n {trainer.intr}")
+            print_color(f"[check] camera extr: \n {trainer.extr}")
 
         if iterations_after > 0:
-            # print(f"[{i+1}/{len(img_paths) - 1}] Rehearshal stage for new points.................")
-            print(f"[{i+1}/{len(img_paths) - 1}] Optimize all .................")
-            if add:
-                trainer.add_gaussians_from_image(gt_image=gt_image, gt_depth=gt_depth, mask=occ_mask)
+            # print_color(f"[{i+1}/{len(img_paths) - 1}] Rehearshal stage for new points.................")
+            print_color(f"[{i+1}/{len(img_paths) - 1}] Optimize all .................")
             frames, frames_center, frames_depth, still_rgb_np, still_center_np, move_rgb_np, move_center_np = trainer.train(
                 iterations=iterations_after,
                 lr=lr_after,
-                lr_camera=lr_camera_after,
-                # lr_camera=0.,
+                # lr_camera=lr_camera_after,
+                lr_camera=0.,
                 save_imgs=True,
                 save_videos=False,
                 save_ckpt=True,
                 ckpt_name=save_name,
                 # fps=fps,
+                lambda_rgb=lambda_rgb,
                 # lambda_depth=0.,
                 lambda_depth=lambda_depth,
                 lambda_var=lambda_var,
                 lambda_still=lambda_still,
+                # lambda_flow=0.,
                 lambda_flow=lambda_flow,
                 densify_times=densify_times_after,
                 densify_interval=densify_interval_after,
+                densify_iter=densify_iter,
                 grad_threshold=grad_threshold_after,
                 # mask=None,
                 mask=occ_mask,
@@ -221,16 +261,16 @@ def main(
                 # slow_means=slow_means,
             )
 
-        print("[check] scales' max, min", trainer.get_attribute("scale").max().item(), trainer.get_attribute("scale").min().item())
-        print("[check] depth_scale", trainer.depth_scale)
-        print("[check] depth far, near", trainer.last_depth[trainer.last_depth>0].max(), trainer.last_depth[trainer.last_depth>0].min())
-        print("[check] camera intr: \n", trainer.intr)
-        print("[check] camera extr: \n", trainer.extr)
-        # print("[check] scales.max", trainer.get_attribute("scale").max())
-        # print("[check] scales.min", trainer.get_attribute("scale").min())
-        # print("[check] far depth", trainer.prev_depth_abs[trainer.prev_depth_abs>0].max())
-        # print("[check] near depth", trainer.prev_depth_abs[trainer.prev_depth_abs>0].min())
-        # print("[check] viewmat\n", trainer.get_viewmat)
+        print_color(f"[check] scales' max, min: {trainer.get_attribute('scale').max().item()} {trainer.get_attribute('scale').min().item()}")
+        # print_color("[check] depth_scale", trainer.depth_scale)
+        print_color(f"[check] depth far, near {trainer.last_depth[trainer.last_depth>0].max()} {trainer.last_depth[trainer.last_depth>0].min()}")
+        print_color(f"[check] camera intr: \n {trainer.intr}")
+        print_color(f"[check] camera extr: \n {trainer.extr}")
+        # print_color("[check] scales.max", trainer.get_attribute("scale").max())
+        # print_color("[check] scales.min", trainer.get_attribute("scale").min())
+        # print_color("[check] far depth", trainer.prev_depth_abs[trainer.prev_depth_abs>0].max())
+        # print_color("[check] near depth", trainer.prev_depth_abs[trainer.prev_depth_abs>0].min())
+        # print_color("[check] viewmat\n", trainer.get_viewmat)
 
         frames_sequence_optimize += frames
         frames_center_sequence_optimize += frames_center
@@ -243,6 +283,17 @@ def main(
             frames_still_center_sequence.append(still_center_np)
             frames_move_sequence.append(move_rgb_np)
             frames_move_center_sequence.append(move_center_np)
+        # render trajectory
+        if traj_num:
+            (out_img, out_img_center, out_img_depth, 
+            out_img_traj, out_img_traj_upon ) = trainer.eval(
+                traj_index=traj_index,
+                line_scale=0.2, 
+                point_scale=2., 
+                alpha=0.8
+            )
+            frames_sequence_traj.append(out_img_traj)
+            frames_sequence_traj_upon.append(out_img_traj_upon)
     
     # generate video
     for name, frames, fps in zip(
@@ -250,6 +301,14 @@ def main(
         [frames_sequence, frames_center_sequence, frames_depth_sequence, frames_sequence_optimize, frames_center_sequence_optimize, frames_depth_sequence_optimize, frames_still_sequence, frames_still_center_sequence, frames_move_sequence, frames_move_center_sequence],
         [5, 5, 5, 30, 30, 30, 5, 5, 5, 5]):
 
+        mp4_path = os.path.join(trainer.dir, f"{name}.mp4")
+        save_video(mp4_path, frames, fps)
+    
+    # generate video for trajectories
+    for name, frames, fps in zip(
+        ["sequence_traj", "sequence_traj_upon"],
+        [frames_sequence_traj, frames_sequence_traj_upon],
+        [5, 5]):
         mp4_path = os.path.join(trainer.dir, f"{name}.mp4")
         save_video(mp4_path, frames, fps)
     
--- gflow/geometry.py
+++ gflow/geometry.py
@@ -9,7 +9,67 @@ import numpy as np
 
 import math
 
+# Adapted from NoPe-NeRF https://github.com/ActiveVisionLab/nope-nerf/blob/main/utils_poses/comp_ate.py
+def rotation_error(pose_error):
+    """Compute rotation error
+    Args:
+        pose_error (4x4 array): relative pose error
+    Returns:
+        rot_error (float): rotation error
+    """
+    a = pose_error[0, 0]
+    b = pose_error[1, 1]
+    c = pose_error[2, 2]
+    d = 0.5*(a+b+c-1.0)
+    rot_error = np.arccos(max(min(d, 1.0), -1.0))
+    return rot_error
 
+def translation_error(pose_error):
+    """Compute translation error
+    Args:
+        pose_error (4x4 array): relative pose error
+    Returns:
+        trans_error (float): translation error
+    """
+    dx = pose_error[0, 3]
+    dy = pose_error[1, 3]
+    dz = pose_error[2, 3]
+    trans_error = np.sqrt(dx**2+dy**2+dz**2)
+    return trans_error
+
+def compute_rpe(gt, pred):
+    trans_errors = []
+    rot_errors = []
+    for i in range(len(gt)-1):
+        gt1 = gt[i]
+        gt2 = gt[i+1]
+        gt_rel = np.linalg.inv(gt1) @ gt2
+
+        pred1 = pred[i]
+        pred2 = pred[i+1]
+        pred_rel = np.linalg.inv(pred1) @ pred2
+        rel_err = np.linalg.inv(gt_rel) @ pred_rel
+        
+        trans_errors.append(translation_error(rel_err))
+        rot_errors.append(rotation_error(rel_err))
+    rpe_trans = np.mean(np.asarray(trans_errors))
+    rpe_rot = np.mean(np.asarray(rot_errors))
+    return rpe_trans, rpe_rot
+
+def compute_ate(gt, pred):
+    """Compute RMSE of ATE (Absolute Trajectory Error)
+    Args:
+        gt: ground-truth translation
+        pred: predicted translation
+    """
+    errors = []
+
+    for pred_xyz, gt_xyz in zip(pred, gt):
+        align_err = gt_xyz - pred_xyz
+        errors.append(np.sqrt(np.sum(align_err ** 2)))
+        
+    ate = np.sqrt(np.mean(np.asarray(errors) ** 2)) 
+    return ate
 
 def xy_grid(W, H, device=None, origin=(0, 0), unsqueeze=None, cat_dim=-1, homogeneous=False, **arange_kw):
     """ Output a (H,W,2) array of int32 
