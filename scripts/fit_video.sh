1dd4e08be091b5514daa20a6e769d48f8c06a7ee
littlepure2333 <rz8217525@163.com>
2024-05-15 21:36:00 +0800

    update running script

--- scripts/fit_video.sh
+++ scripts/fit_video.sh
@@ -1,19 +1,19 @@
 # count=0
 # while [ $count -lt 20 ]; do
-CUDA_VISIBLE_DEVICES=7 python gflow/fit_video.py \
---num_points 20000 --resize 480 \
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python gflow/fit_video.py \
+--num_points 40000 --resize 480 \
 --lr 4e-3 --lr_after 1e-3 \
 --lr_camera 0.00 --lr_camera_after 1e-4 \
---camera_first --iterations_camera 100 \
---iterations_first 1000 --iterations_after 800 \
---densify_interval 200 --grad_threshold 5e-3 --densify_times 2 \
---densify_interval_after 5 --grad_threshold_after 1e-4 --densify_times_after 1 \
---lambda_depth 0.01 --lambda_var 0. --lambda_still 0.001 --lambda_flow 0.01 \
---depth_scale 1.0 --depth_offset 1. \
---background "black" --frame_range 20 --frame_start 20 \
---skip_interval 1 --no_add \
+--camera_first --iterations_camera 150 \
+--iterations_first 500 --iterations_after 300 \
+--densify_interval 150 --grad_threshold 5e-3 --densify_times 2 \
+--densify_interval_after 100 --grad_threshold_after 1e-4 --densify_times_after 1 \
+--lambda_rgb 1. --lambda_depth 0.0001 --lambda_var 0. --lambda_still 1. --lambda_flow 0.01 \
+--background "black" --frame_range 20 --frame_start 0 \
+--skip_interval 1 --traj_num 100 --traj_offset 2 \
 --sequence_path "./images/car-turn/car-turn"
-# --sequence_path "./images/hike/hike"
+# --sequence_path "./images/rollerblade/rollerblade"
+# --sequence_path "./images/train/train"
 # --sequence_path "./images/paragliding/paragliding"
 # --sequence_path "/home/wangshizun/projects/gsplat/images/train/train"
 # --sequence_path "/home/wangshizun/projects/gsplat/images/paragliding/paragliding"
