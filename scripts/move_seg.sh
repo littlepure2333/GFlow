70b5948d4c8bb4b50988d20b390fe41fcfd01c7d
littlepure2333 <rz8217525@163.com>
2024-08-09 00:28:36 +0800

    update move_seg.sh

---
+++ scripts/move_seg.sh
@@ -0,0 +1,58 @@
+# conda activate sam
+
+# CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/car-turn/car-turn
+
+
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/bear/bear
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/blackswan/blackswan
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/bmx-bumps/bmx-bumps
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/bmx-trees/bmx-trees
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/boat/boat
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/breakdance/breakdance
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/breakdance-flare/breakdance-flare
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/bus/bus
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/camel/camel
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/car-roundabout/car-roundabout
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/car-shadow/car-shadow
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/car-turn/car-turn
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/car-turn-1080/car-turn-1080
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/cows/cows
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/dance-jump/dance-jump
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/dance-twirl/dance-twirl
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/dog/dog
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/dog-agility/dog-agility
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/drift-chicane/drift-chicane
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/drift-straight/drift-straight
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/drift-turn/drift-turn
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/elephant/elephant
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/flamingo/flamingo
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/goat/goat
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/hike/hike
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/hockey/hockey
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/horsejump-high/horsejump-high
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/horsejump-low/horsejump-low
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/kite-surf/kite-surf
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/kite-walk/kite-walk
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/libby/libby
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/lucia/lucia
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/mallard-fly/mallard-fly
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/mallard-water/mallard-water
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/motocross-bumps/motocross-bumps
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/motocross-jump/motocross-jump
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/motorbike/motorbike
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/paragliding/paragliding
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/paragliding-launch/paragliding-launch
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/parkour/parkour
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/rhino/rhino
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/rollerblade/rollerblade
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/scooter-black/scooter-black
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/scooter-gray/scooter-gray
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/soapbox/soapbox
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/soccerball/soccerball
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/stroller/stroller
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/surf/surf
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/swing/swing
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/tennis/tennis
+CUDA_VISIBLE_DEVICES=1 python utility/move_seg.py --img_dir data_share/DAVIS/images/train/train
+
+# conda deactivate
